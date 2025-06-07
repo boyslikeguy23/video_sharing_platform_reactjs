9fd437c8d30089265f70f7cefa30a37748f926d3
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-06-07 20:26:09 +0700

    fix: change /direct to /chat for compatile with chat url

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
     else if(tab==="Tạo Reels"){
       handleOpenCreateReelModal()
     } else if (tab === "Tin nhắn") {
-      navigate("/direct");
+      navigate("/chat");
     }
     if (tab === "Tìm kiếm") {
       setIsSearchBoxVisible(true);
