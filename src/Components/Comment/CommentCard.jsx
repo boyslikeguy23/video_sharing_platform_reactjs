47ebb9d617cfea9e4dbdaa14d7290667c0cbf862
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-06-13 14:06:15 +0700

    fix: fix unLikeComment not change state when unlike

--- src/Components/Comment/CommentCard.jsx
+++ src/Components/Comment/CommentCard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { isCommentLikedByUser, timeDifference } from "../../Config/Logic";
-import { deleteComment, likeComment } from "../../Redux/Comment/Action";
+import { deleteComment, likeComment, unLikeComment } from "../../Redux/Comment/Action";
 import { BsEmojiSmile, BsPencil, BsThreeDots } from "react-icons/bs";
 import EditCommentModal from "./EditCommentModal";
 import { MdDelete } from "react-icons/md";
@@ -32,7 +32,7 @@ const CommentCard = ({ comment }) => {
   };
 
   const handleUnLikeComment = () => {
-    dispatch(likeComment({ jwt, commentId: comment.id }));
+    dispatch(unLikeComment({ jwt, commentId: comment.id }));
     setIsCommentLike(false);
     setCommentLikes(commentLikes - 1);
   };
