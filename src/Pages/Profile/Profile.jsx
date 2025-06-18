98243d0a6759d9b244270902f70cbab9ea8cbc2f
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-06-18 22:56:09 +0700

    refactor: add post and isRequser to pass in ProfilePostsPart

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate, useLocation } from 'react-router-dom'
 import ProfilePostsPart from '../../Components/ProfilePageCard/ProfilePostsPart'
 import UserDetailCard from '../../Components/ProfilePageCard/UserDetailCard'
 import { isFollowing, isReqUser } from '../../Config/Logic'
 import { findByUsernameAction, getUserProfileAction } from '../../Redux/User/Action'
+import { usePostData } from '../../Hooks/usePostData'
+import CommentModal from '../../Components/Comment/CommentModal'
 
 const Profile = () => {
   const dispatch=useDispatch();
   const token = localStorage.getItem("token");
   const {username} = useParams();
-  const {user}=useSelector(store=>store);
-
-  const isRequser=isReqUser(user.reqUser?.id,user.findByUsername?.id);
+  const {user, post }=useSelector((store)=>store);
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isRequser=isReqUser(user.reqUser?.id,user.findByUsername?.id); // hàm kiểm tra xem có phải mình hay người khác
   const isFollowed=isFollowing(user.reqUser,user.findByUsername);
   console.log(user)
 
@@ -34,8 +38,10 @@ const Profile = () => {
             <UserDetailCard user={profileUser} isFollowing={isFollowed} isRequser={isRequser}/>
         </div>
         <div>
-            <ProfilePostsPart user={profileUser}/>
+            <ProfilePostsPart user={profileUser} posts={post.reqUserPost} isRequser={isRequser}
+ />
         </div>
+       
     </div>
   )
 }
